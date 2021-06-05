a595c38a50a022621cb2251ac3a44bcec71597a5
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-06-05 11:10:00 +0600

    button toggled and nested data accessed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [likeBtnColor,setLikeBtnColor] = useState('');
+  const [users,setUser] = useState([])
+  const [randomUser,setRandomUser] = useState({})
+  const handleLikeBtn = ()=>{
+    const color = likeBtnColor? "":"disabled";
+    setLikeBtnColor(color)
+  }
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(data =>setUser(data))
+    // random user data 
+    fetch('https://randomuser.me/api/')
+    .then(response=>response.json())
+    .then(data =>setRandomUser(data.results[0]))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick={handleLikeBtn} color = {likeBtnColor} ></ThumbUpAltIcon>
+     {
+      users.map(usr=><li><h2>{usr.name}</h2></li>)
+      }
+{
+      <h2>Gender:{randomUser.name?.first}</h2>
+
+}
     </div>
   );
 }
